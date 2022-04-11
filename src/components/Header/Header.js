05bfa583cdff445759cda33b5d63ffca93b88a7d
Soyeb <tssoyeb@gmail.com>
2022-04-11 19:25:08 +0600

    register component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,10 @@ const Header = () => {
         <div className='header'>
             <nav>
                 <Link to="/">Home</Link>
+                <Link to="/register">Register</Link>
                 <Link to="/login">Login</Link>
+               
+                
             </nav>
         </div>
     );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h1>
-            Login Page
-            </h1>
+            <h1>Please Login</h1>
         </div>
     );
 };
