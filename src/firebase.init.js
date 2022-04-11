76936652b18af36d3e1ddfd7e554ae7be96ef820
Soyeb <tssoyeb@gmail.com>
2022-04-11 11:42:33 +0600

    setup configuration

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgmaod1KpHIPqTYPBXzWg8iKtkqTfr_Qg",
+  authDomain: "router-firebase-integrat-227f0.firebaseapp.com",
+  projectId: "router-firebase-integrat-227f0",
+  storageBucket: "router-firebase-integrat-227f0.appspot.com",
+  messagingSenderId: "551868982074",
+  appId: "1:551868982074:web:cd2b3b5bd64033427ca603"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app ;
