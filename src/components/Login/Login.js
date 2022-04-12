2da27bfd460ec45ec47bb5498c17fe4c82ccf2eb
Soyeb <tssoyeb@gmail.com>
2022-04-12 11:12:38 +0600

    added sign in from google from react firebase hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,16 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import app from '../../firebase.init';
 
+const auth =getAuth(app)
 const Login = () => {
-    const {signInwithGoogle} =useFirebase()
+    const [signInwithGoogle]= useSignInWithGoogle(auth)
     return (
         <div>
             <h1>Please Login</h1>
                 <div style={{"margin":"20px"}}>
-                    <button onClick={signInwithGoogle}>Sign In with Google</button>
+                    <button onClick={()=>signInwithGoogle()}>Sign In with Google</button>
                 </div>
             <form >
                 
---
+++ src/components/Products/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h1>Who is there ?</h1>
+        </div>
+    );
+};
+
+export default Products;
