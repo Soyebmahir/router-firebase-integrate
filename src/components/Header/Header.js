270caab33c1bd592d271e44bd3bd27c6aaae0dc4
Soyeb <tssoyeb@gmail.com>
2022-04-12 11:20:31 +0600

    added signOut

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
+import { getAuth, signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
-import './Header.css'
+import app from '../../firebase.init';
 
+import './Header.css'
+const auth = getAuth(app)
 const Header = () => {
-    const {user,handleSignOut}=useFirebase()
+    const [user]=useAuthState(auth)
     return (
         <div className='header'>
             <nav>
@@ -16,7 +19,7 @@ const Header = () => {
                {
                    user?.uid
                    ?
-                   <button onClick={handleSignOut}>Sign Out</button>
+                   <button onClick={()=>signOut(auth)}>Sign Out</button>
                    :
                 <Link to="/login">Login</Link>
                }
